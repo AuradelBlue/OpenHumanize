f346f7933e68c1846cf8d1d562eda3345270fba2
AuradelBlue <JazzFuller.biz@gmail.com>
2024-05-12 00:09:31 -0700

    Enhanced error handling for spaCy model loading

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 from utils import install_spacy_model, load_spacy_model
 import routes  # Make sure to import the routes module
 
 app = Flask(__name__, static_url_path='/static')
 
 # Preload the spaCy model at startup
-install_spacy_model()  # Ensure the model is installed
-nlp_model = load_spacy_model()  # Load the model
+try:
+    install_spacy_model()  # Ensure the model is installed
+    nlp_model = load_spacy_model() # Load the model
+except Exception as e:
+    app.logger.error(\"Failed to install or load spaCy model: {}\".format(e))
+    # We could choose to halt the app or leave it running with limited functionality
+    nlp_model = None
 
 # Configure routes and pass the loaded nlp_model
-routes.configure_routes(app, nlp_model)
+if nlp_model:
+    routes.configure_routes(app, nlp_model)
+else:
+    @app.route('/')
+    def error():
+        return jsonify({"error: \"The NPP model could not be loaded, please check the server logs for more details.\"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
